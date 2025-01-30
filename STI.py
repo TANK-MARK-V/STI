fa4800815926be09c131b2b85b90c54d02162837
Марк <150333996+TANK-MARK-V@users.noreply.github.com>
2025-01-30 19:46:45 +0700

    Update STI.py
    Исправлены баги с импликацией и эквиваленцией

--- STI.py
+++ STI.py
@@ -49,7 +49,14 @@ def changing(value):  # Автоматически заменяет знаки
     for put, take in dct.items():
         for get in take:
             new_value = new_value.replace(get, put) if get in new_value else new_value
+    if ' <= ' in new_value and 'not' in new_value:
+        new_value = new_value.split(' <= ')
+        new_value = f'not {new_value[0]} or {new_value[1]}'
+    if ' == ' in new_value and 'not' in new_value:
+        new_value = new_value.split(' == ')
+        new_value = f'({new_value[0]}) == ({new_value[1]})'
     return new_value
+
 
 def preps(rest, dct, value):
     new_value = value
@@ -95,26 +102,35 @@ def sti(primer, only=''):
         need = None
     first_space = "   "
     second_space = " "
-    out = ''
-    out += 'x y z w'[:num_of_perem * 2] + "  " + first_space.join([str(i + 1) for i in range(len(DO.keys())) if i < 9])
-    if len(DO.keys()) >= 10:
-        out += second_space + second_space.join([str(i + 1) for i in range(len(DO.keys())) if i >= 9])
+    out = 'x y z w'[:num_of_perem * 2] + "  "
+    if len(DO.keys()) == 1:
+        out += 'F'
+    else:
+        out += first_space.join([str(i) for i in range(1, len(DO.keys())) if i < 10])
+        if len(DO.keys()) >= 10:
+            out += second_space + second_space.join([str(i) for i in range(len(DO.keys())) if i >= 10])
+        out += first_space + 'F'
     out += '\n'
     for x in range(2):
-        for y in range(2):
-            if num_of_perem > 2:
-                for z in range(2):
-                    if num_of_perem > 3:
-                        for w in range(2):
-                            sleest = count(DO, x=x, y=y, z=z, w=w)
+        if num_of_perem > 1:
+            for y in range(2):
+                if num_of_perem > 2:
+                    for z in range(2):
+                        if num_of_perem > 3:
+                            for w in range(2):
+                                sleest = count(DO, x=x, y=y, z=z, w=w)
+                                if not need_smth or (need_smth and int(sleest[-1]) == need):
+                                    out += f"{x} {y} {z} {w}  " + first_space.join(sleest) + "\n"
+                        else:
+                            sleest = count(DO, x=x, y=y, z=z)
                             if not need_smth or (need_smth and int(sleest[-1]) == need):
-                                out += f"{x} {y} {z} {w}  " + first_space.join(sleest) + "\n"
-                    else:
-                        sleest = count(DO, x=x, y=y, z=z)
-                        if not need_smth or (need_smth and int(sleest[-1]) == need):
-                            out += f"{x} {y} {z}  " + first_space.join(sleest) + "\n"
-            else:
-                sleest = count(DO, x=x, y=y)
-                if not need_smth or (need_smth and int(sleest[-1]) == need):
-                    out += f"{x} {y}  " + first_space.join(sleest) + "\n"
+                                out += f"{x} {y} {z}  " + first_space.join(sleest) + "\n"
+                else:
+                    sleest = count(DO, x=x, y=y)
+                    if not need_smth or (need_smth and int(sleest[-1]) == need):
+                        out += f"{x} {y}  " + first_space.join(sleest) + "\n"
+        else:
+            sleest = count(DO, x=x)
+            if not need_smth or (need_smth and int(sleest[-1]) == need):
+                out += f"{x}  " + first_space.join(sleest) + "\n"
     return out
